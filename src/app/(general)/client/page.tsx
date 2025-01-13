8be7271cb874a7c51be2cde42cc10840bc624109
PhongNDH <phongndhdev@gmail.com>
2025-01-13 17:31:14 +0700

    upd : some import

--- src/app/(general)/client/page.tsx
+++ src/app/(general)/client/page.tsx
@@ -1,24 +1,12 @@
 "use client"
 
 import React from "react";
-import {FreelancerSearch} from "@/app/client/home/freelancer-search";
-import {InspirationGallery} from "@/app/client/home/inspiration-gallery";
 import {FeaturedCareers} from "@/components/constitution/featured-career";
 import {RelatedJobs} from "@/components/constitution/job-category";
-import {WelcomeBanner} from "@/app/client/home/welcome-banner";
 
 
 const HomePage = () => {
     return <div className="mt-3 mb-10">
-        <div className="max-width-suitable px-14 mx-auto">
-            <WelcomeBanner/>
-        </div>
-        <div className="max-width-suitable px-14 mx-auto">
-            <FreelancerSearch/>
-        </div>
-        <div className="max-width-suitable px-14 mx-auto">
-            <InspirationGallery/>
-        </div>
         <div className="bg-black mt-16">
             <div className="max-width-suitable px-14 mx-auto">
                 <FeaturedCareers/>
--- src/app/(general)/freelancer/page.tsx
+++ src/app/(general)/freelancer/page.tsx
@@ -1,8 +1,6 @@
 "use client"
 
 import React from "react";
-import {JobSearch} from "@/app/freelancer/home/job-search";
-import {ApplicationProcess} from "@/app/freelancer/home/application-process";
 import {FeaturedCareers} from "@/components/constitution/featured-career";
 import {RelatedJobs} from "@/components/constitution/job-category";
 import {ImageCarousel} from "@/components/constitution/carousel-indicator";
@@ -11,12 +9,6 @@ const HomePage = () => {
     return <div className="mt-3 mb-10">
         <div className="max-width-suitable px-14 mx-auto">
             <ImageCarousel/>
-        </div>
-        <div className="max-width-suitable px-14 mx-auto">
-            <ApplicationProcess/>
-        </div>
-        <div className="max-width-suitable px-14 mx-auto">
-            <JobSearch/>
         </div>
         <div className="bg-black mt-16">
             <div className="max-width-suitable px-14 mx-auto">
