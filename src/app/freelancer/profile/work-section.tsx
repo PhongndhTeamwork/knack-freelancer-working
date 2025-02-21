57a526020285c0d61ba34191e0f0008d544eb0fa
PhongNDH <phongndhdev@gmail.com>
2025-02-21 19:28:24 +0700

    üpdate icon

--- src/app/freelancer/profile/work-section.tsx
+++ src/app/freelancer/profile/work-section.tsx
@@ -52,67 +52,6 @@ export const WorkSection = () => {
                                       url="/freelancer/profile/Coins.svg"/>
                         <h3 className="font-bold responsive-text-20">Bảng giá</h3>
                     </div>
-                    {/*<div className="grid md:grid-cols-2 md:gap-4">*/}
-                    {/*    <Card className="p-4 space-y-5 h-fit">*/}
-                    {/*        <h3 className="font-bold responsive-text-16">Mẫu ảnh</h3>*/}
-                    {/*        {priceList.map((p, index) => (*/}
-                    {/*            <div key={index} className="space-y-2">*/}
-                    {/*                <div className="flex justify-between items-center">*/}
-                    {/*                    <Button variant="secondary" size="sm"*/}
-                    {/*                            className="rounded-md h-8 border shadow text-[#333333]">*/}
-                    {/*                        {p.type}*/}
-                    {/*                        <ArrowDownCircle className="h-5 w-5"/>*/}
-                    {/*                    </Button>*/}
-                    {/*                    <span className="font-bold text-[#333333]">*/}
-                    {/*                        {p.price.toLocaleString("de-DE")} VNĐ*/}
-                    {/*                    </span>*/}
-                    {/*                </div>*/}
-                    {/*                <div className="text-[#545454] responsive-text-16" style={{lineHeight : "24px"}}>*/}
-                    {/*                    {p.description}*/}
-                    {/*                </div>*/}
-                    {/*            </div>))}*/}
-                    {/*    </Card>*/}
-                    {/*    <div className="space-y-4">*/}
-                    {/*        <Card className="p-4 space-y-5 h-fit">*/}
-                    {/*            <h3 className="font-bold responsive-text-16">Stylish</h3>*/}
-                    {/*            {priceList.map((p, index) => (<div key={index} className="space-y-2">*/}
-                    {/*                <div className="flex justify-between items-center">*/}
-                    {/*                    <Button variant="secondary" size="sm"*/}
-                    {/*                            className="rounded-md border h-8 shadow text-[#333333]">*/}
-                    {/*                        {p.type}*/}
-                    {/*                        <ArrowDownCircle className="h-5 w-5"/>*/}
-                    {/*                    </Button>*/}
-                    {/*                    <span className="font-bold text-[#333333] responsive-text-16" style={{lineHeight : "24px"}}>*/}
-                    {/*            {p.price.toLocaleString("de-DE")} VNĐ*/}
-                    {/*        </span>*/}
-                    {/*                </div>*/}
-                    {/*                <div className="text-[#545454]">*/}
-                    {/*                    {p.description}*/}
-                    {/*                </div>*/}
-
-                    {/*            </div>))}*/}
-                    {/*        </Card>*/}
-                    {/*        <Card className="p-4 space-y-5 h-fit">*/}
-                    {/*            <h3 className="font-bold responsive-text-16">Makeup Artist</h3>*/}
-                    {/*            {priceList.map((p, index) => (<div key={index} className="space-y-2">*/}
-                    {/*                <div className="flex justify-between items-center">*/}
-                    {/*                    <Button variant="secondary" size="sm"*/}
-                    {/*                            className="rounded-md h-8 border shadow text-[#333333]">*/}
-                    {/*                        {p.type}*/}
-                    {/*                        <ArrowDownCircle className="h-5 w-5"/>*/}
-                    {/*                    </Button>*/}
-                    {/*                    <span className="font-bold text-[#333333]">*/}
-                    {/*            {p.price.toLocaleString("de-DE")} VNĐ*/}
-                    {/*        </span>*/}
-                    {/*                </div>*/}
-                    {/*                <div className="text-[#545454]">*/}
-                    {/*                    {p.description}*/}
-                    {/*                </div>*/}
-
-                    {/*            </div>))}*/}
-                    {/*        </Card>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
                 </div>
             </Card>
 
@@ -122,18 +61,6 @@ export const WorkSection = () => {
                                   url="/freelancer/profile/BriefCase.svg"/>
                     <CardTitle style={{marginTop: 0}} className="responsive-text-20">Portfolio của bạn</CardTitle>
                 </CardHeader>
-                {/*<CardContent>*/}
-                {/*    <div className="space-y-2">*/}
-                {/*        /!*<div className="flex justify-between items-start">*!/*/}
-                {/*        /!*    <h3 className="font-medium">Người mẫu triển vọng</h3>*!/*/}
-                {/*        /!*    <span className="text-sm text-muted-foreground">7/2024 - 12/2025</span>*!/*/}
-                {/*        /!*</div>*!/*/}
-                {/*        /!*<p className="text-sm text-muted-foreground">*!/*/}
-                {/*        /!*    Làm việc ở đây tốt nhưng mà không có sướng. Sếp không trả lương đúng hạn. Tóm lại là không*!/*/}
-                {/*        /!*    cần nhắc làm ở đây thì khi vì quá.*!/*/}
-                {/*        /!*</p>*!/*/}
-                {/*    </div>*/}
-                {/*</CardContent>*/}
             </Card>
         </div>
 
